d33e8469d4cc6d15de1d1e33f343f18c338df08c
Harold Iedema <harold@iedema.me>
2019-03-09 13:10:43 +0100

    app

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const app   = require("electron").app;
+const https = require("https");
+
+
+function doGetRequest(url)
+{
+    console.log(`Fetching "${url}"...`);
+
+    return new Promise((resolve) => {
+        let data = [];
+        https.get(url, (res) => {
+            res.setEncoding("binary");
+            res.on("data", (chunk) => data.push(Buffer.from(chunk, "binary")));
+            res.on("end", () => {
+                let d = Buffer.concat(data).toString();
+                console.log(`Received ${d.length} bytes.`);
+                resolve(d);
+            });
+        });
+    });
+}
+
+(async () => {
+    await app.whenReady();
+
+    // Fetch something... Doesn't matter what it is.
+    let html = await doGetRequest('https://api.chucknorris.io/jokes/random');
+
+    // Now we wait... Since Electron 5.x, the application will crash with exit code 127 after a couple of seconds.
+    // This time usually ranges between 5 and 20 seconds. I think it happens when the socket connection that is used
+    // internally by {http(s)} is cleaned up or closed.
+    console.log('And now we wait for a few seconds...');
+
+})();
